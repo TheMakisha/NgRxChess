5384fcbc5e3b888f606b8c555abba312983424d1
unknown <matijasavic3@gmail.com>
2025-12-12 17:25:47 +0100

    Dodata navigacija, MatchHistory i Friends komponenta

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MatchHistoryComponent } from './match-history/match-history.component';
+import { FriendsComponent } from './friends/friends.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'match-history',
+    component: MatchHistoryComponent
+  },
+  {
+    path: 'friends',
+    component: FriendsComponent
+  }
+];
